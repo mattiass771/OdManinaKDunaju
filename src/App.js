ff5b0274f4bd93ab48d093bf2182b940d3b911ee
mattiass771 <>
2021-05-25 07:15:53 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Typography variant="h1" component="h1">
+          h1. Heading
+        </Typography>
+      </header>
+    </div>
+  );
+}
+
+export default App;
